2a7467d28d59638dc3f5db492e8aefad9fd5a83b
Gleilson Ferreira <fgleilsonf@gmail.com>
2022-09-03 16:03:11 -0300

    Feat: Ajustes textos

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,13 +9,13 @@ const About = () => {
             <img src="assets/img/bggg.jpg" className="img-fluid" alt=""/>
           </div>
           <div className="col-lg-6 pt-4 pt-lg-0">
-            <h1><strong>Pagamento Único, e seu link de acesso ja e liberado no sistema fidelidade Kamarote
+            <h1><strong>Pagamento Único, e seu link de acesso já é liberado no sistema fidelidade Kamarote
               Digital.</strong></h1>
             <p className="fst-italic">
 
             </p>
             <ul>
-              <li>Ative seu QRCODE direto no App e use seu cupon de desconto disponível em nossos parceiros
+              <li>Ative seu QRCODE direto no App e use seu cupom de desconto disponível em nossos parceiros
                 credenciados
               </li>
             </ul>
