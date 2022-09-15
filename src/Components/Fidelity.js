1bc6fba6bc2dfe498d56fe0a94e20cbf4459f97a
denismendes7 <denismendes250@gmail.com>
2022-09-14 21:59:00 -0300

    Appstory

--- src/Components/Fidelity.js
+++ src/Components/Fidelity.js
@@ -1,11 +1,19 @@
 import React from "react";
 import imag from "./../assets/img/imag.png";
+import app from "./../assets/img/app.png";
+import story from "./../assets/img/story.png";
 
 const Fidelity = () => {
   return (
     <section>
           <div className="roww">
             <img src={imag} className="img-fluid" alt=""/>
+      </div>
+      <div className="imgapp">
+      <button onclick="()">
+      <img src={app} /></button>
+      <button onclick="()">
+      <img src={story} /></button>
       </div>
     </section>
   )
