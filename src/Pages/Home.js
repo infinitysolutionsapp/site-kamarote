a182e27907de2773a873173186b0701fa63a62b3
Gleilson Ferreira <fgleilsonf@gmail.com>
2022-11-28 11:11:32 -0300

    Feat: Link loja

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,8 +11,23 @@ import BannerHome from "Components/BannerHome/BannerHome";
 import OpenAccount from "Components/OpenAccount";
 import BannerHorizontal from "Components/BannerHorizontal";
 import LinkApps from "Components/LinkApps";
+import AppBanner from "smart-app-banner-react";
+import Platform from 'react-platform-js';
 
 function Home() {
+
+  const appInfo = {
+    imageUrl:
+      'https://play-lh.googleusercontent.com/vBRfpFgUc9N2FtWaVFIL-Bu0S8KLhKQE7JFHZDlqul99vzY9kV9RJdq1XVH5lzI5Og=w240-h480-rw',
+    name: 'Kamarote Digital',
+    publisher: 'Disponível nas lojas',
+    infoText: 'Baixe agora!',
+    linkButtonText: 'VER',
+    linkUrl: Platform.OS === 'iOS' ?
+      'https://apps.apple.com/us/app/kamarote-digital/id1644390610'
+      : 'https://play.google.com/store/apps/details?id=com.infinitysolutionsapp.kamarotedigitalapp'
+  }
+
   return (
     <Fragment>
       <BannerHome/>
@@ -29,6 +44,17 @@ function Home() {
         <Contact/>
       </main>
       <Footer/>
+
+      <div>
+        <AppBanner
+          appInfo={appInfo}
+          position='bottom'
+          hidden={false}
+          style={{
+            zIndex: 10
+          }}
+        />
+      </div>
     </Fragment>
   );
 }
